06a52598091251d9f874cbadf7964e618c94a1e4
bogdanovso <inostc@bk.ru>
2020-08-28 15:57:37 +0300

    Updates

---
+++ precache-manifest.ce169d999ee632e6ea4e4b272265b2f9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a71512fc8ab25cfa1888fdd23f62d5",
+    "url": "/kitty-eats/index.html"
+  },
+  {
+    "revision": "5f322ddf4976c41f010e",
+    "url": "/kitty-eats/static/css/main.c26ce1df.chunk.css"
+  },
+  {
+    "revision": "c7ae0ad8a59769ad0061",
+    "url": "/kitty-eats/static/js/2.58276863.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/kitty-eats/static/js/2.58276863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f322ddf4976c41f010e",
+    "url": "/kitty-eats/static/js/main.873dbcd3.chunk.js"
+  },
+  {
+    "revision": "dd02245fcd1cdfd5c275",
+    "url": "/kitty-eats/static/js/runtime-main.fa36bb38.js"
+  },
+  {
+    "revision": "1791beb051f42fd1e19c38596d9207dc",
+    "url": "/kitty-eats/static/media/Exo2.0-Thin.1791beb0.ttf"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/kitty-eats/static/media/Exo2.0-Thin.44bde874.svg"
+  },
+  {
+    "revision": "57ef0e611efd3124f1308e2aa46d1dde",
+    "url": "/kitty-eats/static/media/Exo2.0-Thin.57ef0e61.eot"
+  },
+  {
+    "revision": "c33b184f5dd1056361d472fd0a43e6a5",
+    "url": "/kitty-eats/static/media/Exo2.0-Thin.c33b184f.woff"
+  },
+  {
+    "revision": "c1d2e8db762d2eeec0cc8b1d2f095e01",
+    "url": "/kitty-eats/static/media/cat.c1d2e8db.png"
+  },
+  {
+    "revision": "8007da0e3cb1ecbc5158b6a81c28a793",
+    "url": "/kitty-eats/static/media/pattern.8007da0e.png"
+  },
+  {
+    "revision": "70ba133fe9fa8b6dfa0e575a2598be11",
+    "url": "/kitty-eats/static/media/trebuc.70ba133f.svg"
+  },
+  {
+    "revision": "cf09781f0904c29ad1f58b6c842aac64",
+    "url": "/kitty-eats/static/media/trebuc.cf09781f.woff"
+  },
+  {
+    "revision": "ea3fdfa648b7ec0e4183621443f549e3",
+    "url": "/kitty-eats/static/media/trebuc.ea3fdfa6.ttf"
+  },
+  {
+    "revision": "f68bcb3ca5482544b6827d339bf95dd5",
+    "url": "/kitty-eats/static/media/trebuc.f68bcb3c.eot"
+  }
+]);
